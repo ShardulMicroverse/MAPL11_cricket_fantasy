7168d1f45ffac5f09227914acc20778ccb90d5fc
atharvan1706 <atharvan@microversedcs.com>
2026-02-12 13:38:30 +0530

    Update PredictionsPage.jsx

--- client/src/pages/PredictionsPage.jsx
+++ client/src/pages/PredictionsPage.jsx
@@ -94,7 +94,7 @@ export default function PredictionsPage() {
           {/* Total Score */}
           <div className="prediction-item">
             <label className="form-label">
-              Total Match Runs (1st + second innings)
+              Total Match Runs (first + second innings)
               <span className="points-badge">+50 / -10</span>
             </label>
             <input
